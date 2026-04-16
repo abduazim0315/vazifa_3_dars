68981fc40d0d70cfd80158241019e0aba5bc9c3c
Abduazim Abdumuxtorov <asadillomurotaliyef@gmail.com>
2026-04-16 19:04:43 +0500

    Update views.py

--- project/main/views.py
+++ project/main/views.py
@@ -6,7 +6,5 @@ def home(request):
     return render(request, 'main/index.html', {'news_list': news_list})
 
 def news_detail(request, news_id):
-    news = get_object_or_404(News, id=news_id)
-    news.views += 1
-    news.save()
+    news = get(id=news_id)
     return render(request, 'main/detail.html', {'news': news})
